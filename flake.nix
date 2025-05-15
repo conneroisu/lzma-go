e0dcd06e26320b3c4c9fe25e1d9d68e1d0e09b82
connerohnesorge <conneroisu@outlook.com>
2025-05-15 11:15:03 -0500

    latest

---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  description = "A development shell for go";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = inputs @ {nixpkgs, ...}: let
+    supportedSystems = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      scripts = {
+        dx = {
+          exec = ''$EDITOR "$REPO_ROOT"/flake.nix'';
+          description = "Edit flake.nix";
+        };
+        gx = {
+          exec = ''$EDITOR "$REPO_ROOT"/go.mod'';
+          description = "Edit go.mod";
+        };
+      };
+
+      scriptPackages =
+        pkgs.lib.mapAttrs
+        (
+          name: script:
+            pkgs.writeShellApplication {
+              inherit name;
+              text = script.exec;
+              runtimeInputs = script.deps or [];
+            }
+        )
+        scripts;
+
+      buildWithSpecificGo = pkg: pkg.override {buildGoModule = pkgs.buildGo124Module;};
+    in {
+      default = pkgs.mkShell {
+        name = "dev";
+
+        # Available packages on https://search.nixos.org/packages
+        packages = with pkgs;
+          [
+            alejandra # Nix
+            nixd
+            statix
+            deadnix
+
+            go_1_24 # Go Tools
+            air
+            templ
+            golangci-lint
+            (buildWithSpecificGo revive)
+            (buildWithSpecificGo gopls)
+            (buildWithSpecificGo templ)
+            (buildWithSpecificGo golines)
+            (buildWithSpecificGo golangci-lint-langserver)
+            (buildWithSpecificGo gomarkdoc)
+            (buildWithSpecificGo gotests)
+            (buildWithSpecificGo gotools)
+            (buildWithSpecificGo reftools)
+            pprof
+            graphviz
+            goreleaser
+            cobra-cli
+          ]
+          ++ builtins.attrValues scriptPackages;
+
+        shellHook = ''
+          export REPO_ROOT=$(git rev-parse --show-toplevel)
+        '';
+      };
+    });
+  };
+}
